d3bd24ac5bf1d8e934a9488c1192dbbcadd0eadc
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2025-03-15 10:50:43 +0700

    ready_for_exam

--- lib/profilecard.dart
+++ lib/profilecard.dart
@@ -7,15 +7,59 @@ class Profilecard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0XFF28283C),
-      body: Center(
-        child: Row(
-          children: [
-            Container(
-              height: 100,
-              color: Colors.amber,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 350,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 83, 83, 145),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 220,
+                left: MediaQuery.of(context).size.width / 2 - 100,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          border:
+                              Border.all(color: Color(0XFF28283C), width: 5),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://i.pinimg.com/736x/6a/fc/5c/6afc5c43a5050054d7482202e3b75239.jpg"))),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "Pakorn Surakarnkul",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Student",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
